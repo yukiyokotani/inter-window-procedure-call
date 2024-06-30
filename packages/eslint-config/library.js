8a50a879b3a3c2bed49bfe9c81ef420b656d1c9a
Yuki Yokotani <yokotani.yuki@gmail.com>
2024-06-30 16:13:52 +0900

    turborepoの初期構築

--- packages/eslint-config/library.js
+++ packages/eslint-config/library.js
@@ -1,34 +1,47 @@
-const { resolve } = require("node:path");
+const { resolve } = require('node:path');
 
-const project = resolve(process.cwd(), "tsconfig.json");
+const project = resolve(process.cwd(), 'tsconfig.json');
 
 /** @type {import("eslint").Linter.Config} */
 module.exports = {
-  extends: ["eslint:recommended", "prettier", "turbo"],
-  plugins: ["only-warn"],
+  extends: ['eslint:recommended', 'prettier', 'turbo'],
+  plugins: ['only-warn', 'import'],
   globals: {
     React: true,
-    JSX: true,
+    JSX: true
   },
   env: {
-    node: true,
+    node: true
   },
   settings: {
-    "import/resolver": {
+    'import/resolver': {
       typescript: {
-        project,
-      },
-    },
+        project
+      }
+    }
   },
   ignorePatterns: [
     // Ignore dotfiles
-    ".*.js",
-    "node_modules/",
-    "dist/",
+    '.*.js',
+    'node_modules/',
+    'dist/'
   ],
   overrides: [
     {
-      files: ["*.js?(x)", "*.ts?(x)"],
-    },
+      files: ['*.js?(x)', '*.ts?(x)']
+    }
   ],
+  rules: {
+    'import/order': [
+      'error',
+      {
+        groups: ['builtin', 'external', 'parent', 'sibling', 'index'],
+        'newlines-between': 'always',
+        alphabetize: {
+          order: 'asc',
+          caseInsensitive: true
+        }
+      }
+    ]
+  }
 };
